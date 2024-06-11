dc5737333b880eee7c31d299b96ca260faedee2d
Jacob Archambault <jacobarchambault@gmail.com>
2024-06-11 17:46:14 -0400

    Add sign_up page

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate 
 
 def create_app(test_config=None):
@@ -19,4 +19,8 @@ def create_app(test_config=None):
     db.init_app(app)
     migrate = Migrate(app, db)
     
+    @app.route('/sign_up')
+    def sign_up():
+        return render_template('sign_up.html')
+
     return app
