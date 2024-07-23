a622933ae8e6dd02f87cf2f89bc09c1834cefd73
Jacob Archambault <jacobarchambault@gmail.com>
2024-07-22 20:40:22 -0400

    Remove sudo from bash podman commands

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-db_host = os.environ.get('DB_HOST', default='localhost')
-db_name = os.environ.get('DB_NAME', default='notes')
-db_user = os.environ.get('DB_USERNAME', default='notes_user')
-db_password = os.environ.get('DB_PASSWORD', default='')
-db_port = os.environ.get('DB_PORT', default='5432')
+db_host = "notes_notes_db_container_1"
+db_name = os.environ.get('DB_NAME')
+db_user = os.environ.get('DB_USERNAME')
+db_password = os.environ.get('DB_PASSWORD')
+db_port = os.environ.get('DB_PORT')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
--- down.sh
+++ down.sh
@@ -1,6 +1,6 @@
 source .env
 echo "stopping any pre-existing db containers..."
-sudo podman container stop $DB_CONTAINER_NAME & sudo podman container rm  $DB_CONTAINER_NAME
+podman container stop $DB_CONTAINER_NAME & podman container rm  $DB_CONTAINER_NAME
 
 echo "removing existing networks"
-sudo podman network rm notes
+podman network rm notes
