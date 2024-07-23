06be2608415ba7926518862a384ae8b6d6b05eaa
Jacob Archambault <jacobarchambault@gmail.com>
2024-07-23 17:42:25 -0400

    Expose port in docker compose instead of publishing it

--- down.sh
+++ down.sh
@@ -1,6 +1,6 @@
 source .env
 echo "stopping any pre-existing db containers..."
-podman container stop $DB_CONTAINER_NAME & podman container rm  $DB_CONTAINER_NAME
+podman container stop $DB_HOST & podman container rm  $DB_HOST
 
 echo "removing existing networks"
 podman network rm notes
--- up.sh
+++ up.sh
@@ -8,7 +8,7 @@ source .env
 
 echo "creating database container"
 podman run -d \
- --name $DB_CONTAINER_NAME \
+ --name $DB_HOST \
  -e POSTGRES_USER=$DB_USERNAME \
  -e POSTGRES_PASSWORD=$DB_PASSWORD \
  -p "5432:5432" \
@@ -17,4 +17,4 @@ podman run -d \
 sleep 8
 
 echo "creating database"
-podman exec -i $DB_CONTAINER_NAME psql -U $DB_USERNAME -c "CREATE DATABASE notes;"
+podman exec -i $DB_HOST psql -U $DB_USERNAME -c "CREATE DATABASE notes;"
