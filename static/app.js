005d24027b3b26818ea12498cb581f654266b6c7
Jacob Archambault <jacobarchambault@gmail.com>
2024-07-25 17:14:58 -0400

    Use let instead of var in app.js

--- static/app.js
+++ static/app.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0),
+  let $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0),
     $deleteIcons = Array.prototype.slice.call(document.querySelectorAll('.delete'), 0);
 
   if ($navbarBurgers.length > 0) {
     $navbarBurgers.forEach(function($el) {
       $el.addEventListener('click', function() {
-        var target = $el.dataset.target,
+        let target = $el.dataset.target,
           $target = document.getElementById(target);
         // Toggle the class on both the "navbar-burger" and the "navbar-menu"
         $el.classList.toggle('is-active');
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
     $deleteIcons.forEach(function($el) {
       console.log("adding event listener", $el);
       $el.addEventListener('click', function() {
-        var target = $el.dataset.target,
+        let target = $el.dataset.target,
           $target = document.getElementById(target);
         $target.remove();
       });
